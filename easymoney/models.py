7f3eac6655d410cf5ae1364b4adc49af7e727d5e
Stepanova Daria <d-step@mail.ru>
2022-12-15 01:13:57 +0700

    refactor db

--- easymoney/models.py
+++ easymoney/models.py
@@ -1,5 +1,5 @@
 from sqlalchemy import Column, Integer, String
-from db import Base, engine
+from easymoney.db import Base, engine
 
 class Operation(Base):
     __tablename__ = 'operations'
@@ -7,8 +7,18 @@ class Operation(Base):
     name = Column(String)
     amount = Column(Integer)
 
+
     def __repr__(self):
         return f'<Operation {self.name} {self.amount}>'
+
+class User(Base):
+    __tablename__ = 'users'
+    id = Column(Integer, primary_key=True)
+    name = Column(String)
+    email = Column(String(120), unique=True)
+
+    def __repr__(self):
+        return f'<User {self.name} {self.email}>'
 
 if __name__ == "__main__":
     Base.metadata.create_all(bind=engine)
