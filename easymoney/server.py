8d76f5e871c8fa3391d24b6742e1a113c66b91d0
Stepanova Daria <d-step@mail.ru>
2022-12-15 00:27:38 +0700

    Blueprint operations

--- easymoney/server.py
+++ easymoney/server.py
@@ -1,25 +1,22 @@
 from flask import Flask, request
 from uuid import uuid4
+from flask import Blueprint
 
-app = Flask(__name__)
+operations_view = Blueprint('operations', __name__)
 
 storage = {}
 
 
-@app.route('/')
-def hello():
-    return 'Hello, World!'
-
-@app.get('/api/v1/operations/')
+@operations_view.get('/')
 def get_all():
     return list(storage.values())
 
-@app.get('/api/v1/operations/<string:uid>')
+@operations_view.get('/<string:uid>')
 def get_by_id(uid: str):
     return storage[uid]
 
 
-@app.post('/api/v1/operations/')
+@operations_view.post('/')
 def add():
     uid = uuid4().hex
     operation = request.json
@@ -27,20 +24,22 @@ def add():
     storage[uid] = operation
     return operation, 201
 
-@app.put('/api/v1/operations/<string:uid>')
+@operations_view.put('/<string:uid>')
 def update(uid: str):
     new_operation = request.json
     new_operation['uid'] = uid
     storage[uid] = new_operation
     return storage[uid]
 
-@app.delete('/api/v1/operations/<string:uid>')
+@operations_view.delete('/<string:uid>')
 def delete(uid: str):
     storage.pop(uid)
     return {}, 204
 
 
 def main():
+    app = Flask(__name__)
+    app.register_blueprint(operations_view, url_prefix='/api/v1/operations')
     app.run()
 
 if __name__ == "__main__":
