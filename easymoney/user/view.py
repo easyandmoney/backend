334c90fd7db049de0a4215288da22cd63d0ddfcf
Daria <d-step@mail.ru>
2022-12-17 13:08:32 +0700

    Aply user methods for storage

--- easymoney/user/view.py
+++ easymoney/user/view.py
@@ -1,37 +1,39 @@
 from flask import request,Blueprint
-from uuid import uuid4
+from dataclasses import asdict
+from easymoney.user.storage import UsersStorage
 
 
 users_view = Blueprint('users', __name__)
 
-storage = {}
+users_storage = UsersStorage()
 
 @users_view.get('/')
 def get_all():
-    return list(storage.values())
+    users = users_storage.get_all()
+    return [asdict(user) for user in users]
 
 @users_view.get('/<string:uid>')
 def get_by_id(uid: str):
-    return storage[uid]
+    return asdict(users_storage.get_by_uid(uid))
 
 @users_view.post('/')
 def add():
-    uid = uuid4().hex
     user = request.json
-    user["uid"] = uid
-    storage[uid] = user
+    user_name = request.json["name"]
+    user_email = request.json["email"]
+    users_storage.add(name=user_name, email=user_email)
     return user, 201
 
 @users_view.put('/<string:uid>')
 def update(uid: str):
-    new_user = request.json
-    new_user['uid'] = uid
-    storage[uid] = new_user
-    return storage[uid]
+    user_name = request.json["name"]
+    user_email = request.json["email"]
+    update_user = users_storage.update(uid=uid, name=user_name, email=user_email)
+    return asdict(update_user)
 
 @users_view.delete('/<string:uid>')
 def delete(uid: str):
-    storage.pop(uid)
-    return {}, 204
+    if users_storage.delete(uid):
+        return {}, 204
 
 
