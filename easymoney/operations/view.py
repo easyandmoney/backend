8519496f9c9cd3835445d7ca63c5339fa92499e2
Nastya <annika-purtova@yandex.ru>
2022-12-17 15:10:12 +0300

    Shift to SQLAlchemy

--- easymoney/operations/view.py
+++ easymoney/operations/view.py
@@ -9,34 +9,29 @@ operations_storage = OperationsStorage()
 @operations_view.get('/')
 def get_all():
     operations = operations_storage.get_all()
-    return [asdict(operation) for operation in operations]
-
+    return [{'uid': operation.id, 'category': operation.name, 'amount': operation.amount} for operation in operations]
 
 @operations_view.get('/<string:uid>')
 def get_by_uid(uid: str):
-    return asdict(operations_storage.get_by_uid(uid))
-
+    operation = operations_storage.get_by_uid(uid)
+    return {'uid': operation.id, 'category': operation.name, 'amount': operation.amount}
 
 @operations_view.post('/')
 def add():
-    operation = request.json
     operation_category = request.json["category"]
     operation_amount = request.json["amount"]
-    operations_storage.add(category=operation_category, amount=operation_amount)
-    return operation, 201
-
+    operation = operations_storage.add(category=operation_category, amount=operation_amount)
+    return {'uid': operation.id, 'category': operation.name, 'amount': operation.amount}, 201
 
 @operations_view.put('/<string:uid>')
 def update(uid: str):
     operation_category = request.json["category"]
     operation_amount = request.json["amount"]
     update_operation = operations_storage.update(uid=uid, category=operation_category, amount=operation_amount)
-    return asdict(update_operation), 201
-
-
+    return {'uid': update_operation.id, 'category': update_operation.name, 'amount': update_operation.amount}, 200
 
 @operations_view.delete('/<string:uid>')
 def delete(uid):
-    if operations_storage.delete(uid):
-        return {}, 204
-
+    if not operations_storage.delete(uid):
+        return {}, 404
+    return {}, 204
