67bcf81fb3fd269d64dfa6fda59c8b265f8f5d01
Daria <d-step@mail.ru>
2022-12-23 16:25:02 +0700

    add connection user->operaton ForeignKey

--- easymoney/models.py
+++ easymoney/models.py
@@ -1,14 +1,20 @@
-from sqlalchemy import Column, Integer, String
+from sqlalchemy import Column, Integer, String, ForeignKey
 from easymoney.db import Base, engine
+from sqlalchemy.orm import relationship
 
 class Operation(Base):
     __tablename__ = 'operations'
     uid = Column(Integer, primary_key=True)
     name = Column(String)
     amount = Column(Integer)
+    user_id = Column(Integer, ForeignKey("users.uid"), nullable=False)
+
+    user = relationship("User", back_populates="operation")
 
     def __repr__(self):
         return f'<Operation {self.name} {self.amount}>'
+
+
 
 class User(Base):
     __tablename__ = 'users'
@@ -16,8 +22,12 @@ class User(Base):
     name = Column(String)
     email = Column(String(120), unique=True)
 
+    operation = relationship("Operation", back_populates="user")
+
     def __repr__(self):
         return f'<User {self.name} {self.email}>'
+
+
 
 if __name__ == "__main__":
     Base.metadata.create_all(bind=engine)
